7e9dccefa19861c70a7cec9714693c7593584cfb
z0kr0m <jan069961@gmail.com>
2024-08-04 12:54:25 -0400

    Hello world

---
+++ 1.Hello-World/hello-world.c
@@ -0,0 +1,11 @@
+/* Hello World in C  */
+
+// Directives
+#include <stdio.h> // Includes standard input / output library 
+
+//Main function where the programn starts.
+int main()
+{
+	printf("Hello world\n "); // Function to print input (included in stdio library)
+	return 0; // Return status code 0 which means success
+}
