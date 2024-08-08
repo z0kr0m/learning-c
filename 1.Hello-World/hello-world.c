02effebdc53c92b2022860d85bc42ed5c8385f40
z0kr0m <jan069961@gmail.com>
2024-08-08 10:29:36 -0400

    edited hello world

--- 1.Hello-World/hello-world.c
+++ 1.Hello-World/hello-world.c
@@ -8,4 +8,5 @@ int main()
 {
 	printf("Hello world\n "); // Function to print input (included in stdio library)
 	return 0; // Return status code 0 which means success
+		  // that's why the return type is int 
 }
